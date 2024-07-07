56413a8f929eea5f3ceb5801e000001264692e30
ivixvi <epitus628@gmail.com>
2024-07-07 10:37:36 +0900

    feat: split by op.Op

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package scimpatch
 
 import (
+	"strings"
+
 	"github.com/elimity-com/scim"
 	"github.com/elimity-com/scim/schema"
 )
@@ -12,5 +14,28 @@ type Patcher struct {
 
 // Apply is returns a argument.
 func (p Patcher) Apply(op scim.PatchOperation, data scim.ResourceAttributes) (scim.ResourceAttributes, error) {
+	switch strings.ToLower(op.Op) {
+	case scim.PatchOperationAdd:
+		return p.Add(op, data)
+	case scim.PatchOperationReplace:
+		return p.Replace(op, data)
+	case scim.PatchOperationRemove:
+		return p.Remove(op, data)
+	}
+	return data, nil
+}
+
+// Add is returns a argument.
+func (p Patcher) Add(op scim.PatchOperation, data scim.ResourceAttributes) (scim.ResourceAttributes, error) {
+	return data, nil
+}
+
+// Replace is returns a argument.
+func (p Patcher) Replace(op scim.PatchOperation, data scim.ResourceAttributes) (scim.ResourceAttributes, error) {
+	return data, nil
+}
+
+// Remove is returns a argument.
+func (p Patcher) Remove(op scim.PatchOperation, data scim.ResourceAttributes) (scim.ResourceAttributes, error) {
 	return data, nil
 }
--- main_test.go
+++ main_test.go
@@ -1,36 +1,89 @@
 package scimpatch_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/elimity-com/scim"
 	"github.com/ivixvi/scimpatch"
+	filter "github.com/scim2/filter-parser/v2"
 )
 
-func TestPatcher_Apply(t *testing.T) {
-	// Create a Patcher instance with a dummy schema
-	patcher := scimpatch.Patcher{}
-
-	// Define a dummy PatchOperation
-	op := scim.PatchOperation{
-		Op:    "add",
-		Value: map[string]interface{}{"displayName": "Alice Green"},
-	}
-
-	// Define the input data
-	data := scim.ResourceAttributes{
-		"displayName": "Bob Green",
-	}
-
-	// Apply the PatchOperation
-	result, err := patcher.Apply(op, data)
+func path(s string) *filter.Path {
+	p, err := filter.ParsePath([]byte(s))
 	if err != nil {
-		t.Fatalf("Apply() returned an unexpected error: %v", err)
+		fmt.Println(fmt.Sprintf("Failed to parse %s occurred by %s", s, err))
+	}
+	return &p
+}
+
+func TestPatcher_Apply(t *testing.T) {
+	// Define the test cases
+	testCases := []struct {
+		name     string
+		op       scim.PatchOperation
+		data     scim.ResourceAttributes
+		expected scim.ResourceAttributes
+	}{
+		{
+			name: "Add operation",
+			op: scim.PatchOperation{
+				Op:    "add",
+				Value: map[string]interface{}{"displayName": "Alice Green"},
+			},
+			data: scim.ResourceAttributes{
+				"displayName": "Bob Green",
+			},
+			expected: scim.ResourceAttributes{
+				"displayName": "Bob Green",
+			},
+		},
+		{
+			name: "Replace operation",
+			op: scim.PatchOperation{
+				Op:    "replace",
+				Value: map[string]interface{}{"displayName": "Alice Green"},
+			},
+			data: scim.ResourceAttributes{
+				"displayName": "Bob Green",
+			},
+			expected: scim.ResourceAttributes{
+				"displayName": "Bob Green",
+			},
+		},
+		{
+			name: "Replace operation",
+			op: scim.PatchOperation{
+				Op:   "remove",
+				Path: path("displayName"),
+			},
+			data: scim.ResourceAttributes{
+				"displayName": "Bob Green",
+			},
+			expected: scim.ResourceAttributes{
+				"displayName": "Bob Green",
+			},
+		},
 	}
 
-	// Check if the result matches the input data
-	// NOTE: now Apply is not implemented. so no change value.
-	if result["displayName"] != "Bob Green" {
-		t.Errorf("expected %v, got %v", data, result)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Log(tc.name)
+			// Create a Patcher instance with a dummy schema
+			patcher := scimpatch.Patcher{}
+
+			// Apply the PatchOperation
+			result, err := patcher.Apply(tc.op, tc.data)
+			if err != nil {
+				t.Fatalf("Apply() returned an unexpected error: %v", err)
+			}
+
+			// Check if the result matches the expected data
+			for key, expectedValue := range tc.expected {
+				if result[key] != expectedValue {
+					t.Errorf("for key %q, expected %v, got %v", key, expectedValue, result[key])
+				}
+			}
+		})
 	}
 }
