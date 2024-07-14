655bd8141cbccddbadace6850d10e868d31ee908
ivixvi <epitus628@gmail.com>
2024-07-14 22:54:26 +0900

    fix: default value

--- walk_expresstion.go
+++ walk_expresstion.go
@@ -4,6 +4,7 @@ import (
 	"github.com/scim2/filter-parser/v2"
 )
 
+// isMatchExpression は、 value が expr の条件に一致するかどうかを確認します
 func isMatchExpression(value map[string]interface{}, expr filter.Expression) bool {
 	switch typedExpr := expr.(type) {
 	case *filter.AttributeExpression:
@@ -16,5 +17,5 @@ func isMatchExpression(value map[string]interface{}, expr filter.Expression) boo
 			return typedExpr.CompareValue == attrValue
 		}
 	}
-	return true
+	return false
 }
