495bc8949401553a9b295c119e7365626cb7024e
ivixvi <epitus628@gmail.com>
2024-07-15 13:41:27 +0900

    feat: tests for path specified add

--- path_specified_add_test.go
+++ path_specified_add_test.go
@@ -19,8 +19,9 @@ func TestPathSpecifiedAdd(t *testing.T) {
 		expected        scim.ResourceAttributes
 		expectedChanged bool
 	}{
+		// Singular Attribute
 		{
-			name: "Add operation",
+			name: "Add operation - Core Singular Attributes - add",
 			op: scim.PatchOperation{
 				Op:    "add",
 				Path:  path(`displayName`),
@@ -31,6 +32,329 @@ func TestPathSpecifiedAdd(t *testing.T) {
 				"displayName": "Alice Green",
 			},
 			expectedChanged: true,
+		},
+		{
+			name: "Add operation - Core Singular Attributes - replace",
+			op: scim.PatchOperation{
+				Op:    "add",
+				Path:  path(`displayName`),
+				Value: "Alice Green",
+			},
+			data: scim.ResourceAttributes{
+				"displayName": "Bob Green",
+			},
+			expected: scim.ResourceAttributes{
+				"displayName": "Alice Green",
+			},
+			expectedChanged: true,
+		},
+		{
+			name: "Add operation - Core Singular Attributes - no changed",
+			op: scim.PatchOperation{
+				Op:    "add",
+				Path:  path(`displayName`),
+				Value: "Alice Green",
+			},
+			data: scim.ResourceAttributes{
+				"displayName": "Alice Green",
+			},
+			expected: scim.ResourceAttributes{
+				"displayName": "Alice Green",
+			},
+			expectedChanged: false,
+		},
+		// Complex Attribute
+		{
+			name: "Add operation - Core Complex Attributes - replace",
+			op: scim.PatchOperation{
+				Op:    "add",
+				Path:  path(`name.familyName`),
+				Value: "Green",
+			},
+			data: scim.ResourceAttributes{
+				"name": map[string]interface{}{
+					"familyName": "Blue",
+					"givenName":  "Alice",
+				},
+			},
+			expected: scim.ResourceAttributes{
+				"name": map[string]interface{}{
+					"familyName": "Green",
+					"givenName":  "Alice",
+				},
+			},
+			expectedChanged: true,
+		},
+		{
+			name: "Add operation - Core Complex Attributes - no value",
+			op: scim.PatchOperation{
+				Op:    "add",
+				Path:  path(`name.familyName`),
+				Value: "Green",
+			},
+			data: scim.ResourceAttributes{
+				"name": map[string]interface{}{
+					"givenName": "Alice",
+				},
+			},
+			expected: scim.ResourceAttributes{
+				"name": map[string]interface{}{
+					"familyName": "Green",
+					"givenName":  "Alice",
+				},
+			},
+			expectedChanged: true,
+		},
+		{
+			name: "Add operation - Core Complex Attributes - no changed.",
+			op: scim.PatchOperation{
+				Op:    "add",
+				Path:  path(`name.familyName`),
+				Value: "Green",
+			},
+			data: scim.ResourceAttributes{
+				"name": map[string]interface{}{
+					"familyName": "Green",
+					"givenName":  "Alice",
+				},
+			},
+			expected: scim.ResourceAttributes{
+				"name": map[string]interface{}{
+					"familyName": "Green",
+					"givenName":  "Alice",
+				},
+			},
+			expectedChanged: false,
+		},
+		{
+			name: "Replace operation - Core Complex Attributes - map specified merge",
+			op: scim.PatchOperation{
+				Op:   "add",
+				Path: path(`name`),
+				Value: map[string]interface{}{
+					"givenName": "Alice",
+					"formatted": "Alice Green",
+				},
+			},
+			data: scim.ResourceAttributes{
+				"name": map[string]interface{}{
+					"familyName": "Green",
+					"formatted":  "Bob Green",
+				},
+			},
+			expected: scim.ResourceAttributes{
+				"name": map[string]interface{}{
+					"familyName": "Green",
+					"givenName":  "Alice",
+					"formatted":  "Alice Green",
+				},
+			},
+			expectedChanged: true,
+		},
+		{
+			name: "Replace operation - Core Singular Attributes - map specified no changed",
+			op: scim.PatchOperation{
+				Op:   "replace",
+				Path: path(`name`),
+				Value: map[string]interface{}{
+					"familyName": "Green",
+					"givenName":  "Alice",
+				},
+			},
+			data: scim.ResourceAttributes{
+				"name": map[string]interface{}{
+					"familyName": "Green",
+					"givenName":  "Alice",
+				},
+			},
+			expected: scim.ResourceAttributes{
+				"name": map[string]interface{}{
+					"familyName": "Green",
+					"givenName":  "Alice",
+				},
+			},
+			expectedChanged: false,
+		},
+		// MultiValued Complex Attribute
+		{
+			name: "Add operation - Core MultiValued Complex Attributes - Add All",
+			op: scim.PatchOperation{
+				Op:   "add",
+				Path: path(`emails`),
+				Value: []interface{}{
+					map[string]interface{}{
+						"type":  "work",
+						"value": "ivixvi-added@example.com",
+					},
+				},
+			},
+			data: scim.ResourceAttributes{
+				"emails": []interface{}{
+					map[string]interface{}{
+						"type":  "home",
+						"value": "ivixvi@example.com",
+					},
+				},
+			},
+			expected: scim.ResourceAttributes{
+				"emails": []interface{}{
+					map[string]interface{}{
+						"type":  "home",
+						"value": "ivixvi@example.com",
+					},
+					map[string]interface{}{
+						"type":  "work",
+						"value": "ivixvi-added@example.com",
+					},
+				},
+			},
+			expectedChanged: true,
+		},
+		{
+			name: "Add operation - Core MultiValued Complex Attributes - Add All no changed",
+			op: scim.PatchOperation{
+				Op:   "add",
+				Path: path(`emails`),
+				Value: []interface{}{
+					map[string]interface{}{
+						"type":  "work",
+						"value": "ivixvi-added@example.com",
+					},
+				},
+			},
+			data: scim.ResourceAttributes{
+				"emails": []interface{}{
+					map[string]interface{}{
+						"type":  "home",
+						"value": "ivixvi@example.com",
+					},
+					map[string]interface{}{
+						"type":  "work",
+						"value": "ivixvi-added@example.com",
+					},
+				},
+			},
+			expected: scim.ResourceAttributes{
+				"emails": []interface{}{
+					map[string]interface{}{
+						"type":  "home",
+						"value": "ivixvi@example.com",
+					},
+					map[string]interface{}{
+						"type":  "work",
+						"value": "ivixvi-added@example.com",
+					},
+				},
+			},
+			expectedChanged: false,
+		},
+		{
+			name: "Add operation - Core MultiValued Complex Attributes - Add For Item",
+			op: scim.PatchOperation{
+				Op:   "add",
+				Path: path(`emails[type eq "work"]`),
+				Value: map[string]interface{}{
+					"type":    "work",
+					"value":   "ivixvi-updated@example.com",
+					"primary": true,
+				},
+			},
+			data: scim.ResourceAttributes{
+				"emails": []interface{}{
+					map[string]interface{}{
+						"type":  "work",
+						"value": "ivixvi@example.com",
+					},
+				},
+			},
+			expected: scim.ResourceAttributes{
+				"emails": []interface{}{
+					map[string]interface{}{
+						"type":    "work",
+						"value":   "ivixvi-updated@example.com",
+						"primary": true,
+					},
+				},
+			},
+			expectedChanged: true,
+		},
+		{
+			name: "Add operation - Core MultiValued Complex Attributes - Add For Item no changed",
+			op: scim.PatchOperation{
+				Op:   "add",
+				Path: path(`emails[type eq "work"]`),
+				Value: map[string]interface{}{
+					"type":  "work",
+					"value": "ivixvi@example.com",
+				},
+			},
+			data: scim.ResourceAttributes{
+				"emails": []interface{}{
+					map[string]interface{}{
+						"type":  "work",
+						"value": "ivixvi@example.com",
+					},
+				},
+			},
+			expected: scim.ResourceAttributes{
+				"emails": []interface{}{
+					map[string]interface{}{
+						"type":  "work",
+						"value": "ivixvi@example.com",
+					},
+				},
+			},
+			expectedChanged: false,
+		},
+		{
+			name: "Add operation - Core MultiValued Complex Attributes - Replace For Attribute",
+			op: scim.PatchOperation{
+				Op:    "add",
+				Path:  path(`emails[type eq "work"].value`),
+				Value: "ivixvi-updated@example.com",
+			},
+			data: scim.ResourceAttributes{
+				"emails": []interface{}{
+					map[string]interface{}{
+						"type":  "work",
+						"value": "ivixvi@example.com",
+					},
+				},
+			},
+			expected: scim.ResourceAttributes{
+				"emails": []interface{}{
+					map[string]interface{}{
+						"type":  "work",
+						"value": "ivixvi-updated@example.com",
+					},
+				},
+			},
+			expectedChanged: true,
+		},
+		{
+			name: "Add operation - Core MultiValued Complex Attributes - Replace For Attribute no changed",
+			op: scim.PatchOperation{
+				Op:    "add",
+				Path:  path(`emails[type eq "work"].value`),
+				Value: "ivixvi@example.com",
+			},
+			data: scim.ResourceAttributes{
+				"emails": []interface{}{
+					map[string]interface{}{
+						"type":  "work",
+						"value": "ivixvi@example.com",
+					},
+				},
+			},
+			expected: scim.ResourceAttributes{
+				"emails": []interface{}{
+					map[string]interface{}{
+						"type":  "work",
+						"value": "ivixvi@example.com",
+					},
+				},
+			},
+			expectedChanged: false,
 		},
 		{
 			name: "Add operation - MultiValued Complex Attributes - Filter & Value addition",
