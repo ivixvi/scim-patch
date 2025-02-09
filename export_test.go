27ca64547b9a34b292b501a4d2811579885bc080
ivixvi <epitus628@gmail.com>
2025-02-09 23:53:30 +0900

    tests: add utils test

--- export_test.go
+++ export_test.go
@@ -15,3 +15,13 @@ func EqMap(m1 map[string]interface{}, m2 map[string]interface{}) bool {
 func MergeMap(m1 map[string]interface{}, m2 map[string]interface{}) (map[string]interface{}, bool) {
 	return mergeMap(m1, m2)
 }
+
+// ContainsMap is export containsMap for testing
+func ContainsMap(slice []map[string]interface{}, item map[string]interface{}) bool {
+	return containsMap(slice, item)
+}
+
+// ContainsItem is export containsItem for testing
+func ContainsItem(slice []interface{}, item interface{}) bool {
+	return containsItem(slice, item)
+}
--- utils_test.go
+++ utils_test.go
@@ -213,3 +213,101 @@ func TestMergeMap(t *testing.T) {
 		})
 	}
 }
+
+// TestContainsMap は containsMap をテストします
+func TestContainsMap(t *testing.T) {
+	// Define the test cases
+	testCases := []struct {
+		name     string
+		slice    []map[string]interface{}
+		item     map[string]interface{}
+		expected bool
+	}{
+		{
+			name:     "empty slice",
+			slice:    []map[string]interface{}{},
+			item:     map[string]interface{}{"key": "value"},
+			expected: false,
+		},
+		{
+			name: "item not in slice",
+			slice: []map[string]interface{}{
+				{"key1": "value1"},
+				{"key2": "value2"},
+			},
+			item:     map[string]interface{}{"key": "value"},
+			expected: false,
+		},
+		{
+			name: "item in slice",
+			slice: []map[string]interface{}{
+				{"key1": "value1"},
+				{"key": "value"},
+			},
+			item:     map[string]interface{}{"key": "value"},
+			expected: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Log(tc.name)
+			// call ContainsMap
+			ok := scimpatch.ContainsMap(tc.slice, tc.item)
+
+			// Check if the result matches the expected data
+			if !(tc.expected == ok) {
+				t.Fatalf("ContainsMap() not returned Expected: %v, %v", tc.expected, ok)
+			}
+		})
+	}
+}
+
+// TestContainsItem は containsItem をテストします
+func TestContainsItem(t *testing.T) {
+	// Define the test cases
+	testCases := []struct {
+		name     string
+		slice    []interface{}
+		item     interface{}
+		expected bool
+	}{
+		{
+			name:     "empty slice",
+			slice:    []interface{}{},
+			item:     "value",
+			expected: false,
+		},
+		{
+			name: "item not in slice",
+			slice: []interface{}{
+				"value1",
+				"value2",
+			},
+			item:     "value",
+			expected: false,
+		},
+		{
+			name: "item in slice",
+			slice: []interface{}{
+				"value1",
+				"value",
+			},
+			item:     "value",
+			expected: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Log(tc.name)
+			// call ContainsItem
+			ok := scimpatch.ContainsItem(tc.slice, tc.item)
+
+			// Check if the result matches the expected data
+			if !(tc.expected == ok) {
+				t.Fatalf("ContainsItem() not returned Expected: %v, %v", tc.expected, ok)
+			}
+		})
+	}
+}
